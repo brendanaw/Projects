bb2568e1d3e418096843c340c5f77dc5893eabed
Brendan Aw <brendanawmingsheng@gmail.com>
2022-01-01 10:28:03 +0800

    Update treasure island.py

--- treasure island.py
+++ treasure island.py
@@ -12,15 +12,12 @@ MM88MMM 8b,dPPYba,  ,adPPYba, ,adPPYYba, ,adPPYba, 88       88 8b,dPPYba,
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
-#Write your code below this line 👇
 first_move = input("You reached a fork in the road, pick left or right.\n").lower()
 
 if first_move== "left":
   second_move = input('You\'ve reached a river and need to get across. Type "swim" to go ahead or "wait" for a boat to arrive.\n').lower()
   if second_move == "wait":
-    third_move = input('You\'ve encounter 3 doors. Pick "Red", "Blue" or "Yellow".\n').lower()
+    third_move = input('You\'ve encountered 3 doors. Pick "Red", "Blue" or "Yellow".\n').lower()
     if third_move == "yellow":
       print("Congrats, you\'ve won!'")
     elif third_move == "red":
